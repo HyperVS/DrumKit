ad9db48dc27903559de8994216fc4c275b85b3b9
HyperVS <63892536+HyperVS@users.noreply.github.com>
2021-04-14 15:39:11 -0400

    Setup express server and upload static files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+// Default options
+app.use(express.static("public"));
+
+// Run server
+const server = app.listen(process.env.PORT || 8080, () => {
+    console.log("Connected and listening on port " + server.address().port);
+});
+
+
